b01370b62f885a308d8ff04edf33d10af9df96e6
Elvis Nogueiras <danvilapps@gmail.com>
2022-11-27 18:49:40 +0000

    Update clienteAxios.js

--- src/config/clienteAxios.js
+++ src/config/clienteAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
   // baseURL: 'https://quiet-eyrie-56964.herokuapp.com/api',
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://onegreetybackend-production.up.railway.app/api',
 
 });
 
